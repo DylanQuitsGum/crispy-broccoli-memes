4913044fecdde48025266e7fe983b76a51e85472
Greg Satterlee <gregory.satterlee@us.af.mil>
2021-01-12 11:38:19 -0600

    changing GSM to gsm

---
+++ src/GSM.IO.Abstractions/IFileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GSM.IO
+{
+    public interface IFileHandler
+    {
+        void CreateFile(string path);
+
+        void CreateFile(string path, bool overwrite);
+
+        void CreateFile(string path, bool overwrite, bool createDirectoryStructure);
+
+        void CreateFolder(string path);
+
+        string CreateTempFile();
+
+        string CreateTempFile(string directory);
+
+        string CreateTempFolder();
+
+        void Compress(string destination, params string[] sourceFiles);
+
+        void Decompress(string sourceFile, string destination);
+    }
+}
