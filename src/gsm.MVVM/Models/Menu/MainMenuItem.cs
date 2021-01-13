25981d9bc62a54d122a4b4942df6239f4b7d8c30
Greg Satterlee <gregory.satterlee@us.af.mil>
2021-01-13 10:50:58 -0600

    Added MaxConcurrentOpen to MainMenuItem

--- src/gsm.MVVM/Models/Menu/MainMenuItem.cs
+++ src/gsm.MVVM/Models/Menu/MainMenuItem.cs
@@ -5,7 +5,20 @@ namespace gsm.MVVM.Models.Menu
 {
     public class MainMenuItem
     {
+        public MainMenuItem()
+        : this(1)
+        {
+
+        }
+
+        public MainMenuItem(int maxConcurrentOpen)
+        {
+            MaxConcurrentOpen = (maxConcurrentOpen > 1) ? maxConcurrentOpen : 1;
+        }
+
         public string DisplayText { get; set; }
+
+        public int MaxConcurrentOpen { get; set; }
 
         public string View { get; set; }
 
---
+++ src/gsm.MVVM/TemplateSelectors/MainMenuDataTemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using gsm.MVVM.Models.Menu;
+
+namespace gsm.MVVM.TemplateSelectors
+{
+    public class MainMenuDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var template = base.SelectTemplate(item, container);
+
+            if (container is FrameworkElement frameworkElement)
+            {
+                if (item is MainMenuItem menuItem)
+                {
+                    if (menuItem.MenuItems.Count > 0)
+                    {
+                        template = frameworkElement.TryFindResource("RootMenuTemplate") as DataTemplate;
+                    }
+                    else
+                    {
+                        template = frameworkElement.TryFindResource("ChildMenuTemplate") as DataTemplate;
+                    }
+                }
+            }
+
+            return template;
+        }
+
+    }
+}
